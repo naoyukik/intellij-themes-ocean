58330e0ca693ebdd0b93625864773b30e400fcd8
GitHub Action <action@github.com>
2023-10-27 05:26:12 +0000

    Template cleanup

--- src/main/kotlin/com/github/naoyukik/intellijthemesocean/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/naoyukik/intellijthemesocean/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.naoyukik.intellijthemesocean.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
