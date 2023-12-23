c82e39669fcfdab0781fcc6831c396bafa26edfc
Kodama Naoyuki <mmaga@dstribe.net>
2023-12-23 21:46:14 +0900

    doc: change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,4 +2,4 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "intellij-themes-ocean"
+rootProject.name = "intellij-themes-calm-waters"
